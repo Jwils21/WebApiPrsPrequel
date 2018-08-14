abb1d03423f968b297a85a838809f4ecbaad39d7
Jonathan Wilson <jon.wilsond@gmail.com>
2018-08-14 10:52:15 -0400

    init

--- WebApiPrsPrequel/Controllers/UsersController.cs
+++ WebApiPrsPrequel/Controllers/UsersController.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.Http;
 using System.Web.Http;
 using WebApiPrsPrequel.Models;
+using WebApiPrsPrequel.Utility;
 
 namespace WebApiPrsPrequel.Controllers
 {
@@ -12,40 +13,175 @@ namespace WebApiPrsPrequel.Controllers
     {
 		private WebApiPrsPrequelContext db = new WebApiPrsPrequelContext();
 
+		//GET-ALL
+		//indicates that a get method will be used to get this info vs. post which updates
 		[HttpGet]
-		public IEnumerable<User> List() {
-			return db.Users.ToList();
+		[ActionName("List")] //this is the name the client will use to call this method
+		public JsonResponse List() {
+			return new JsonResponse {
+				Data = db.Users.ToList()
+			};
 		}
+
+		//GET-ONE
 		[HttpGet]
-		public User Get(int? id) {
-			if(id == null) return null;
-			return db.Users.Find(id);
+		[ActionName("Get")]
+		public JsonResponse User (int? id) {
+			if(id == null) {
+				return new JsonResponse {
+					Result = "Failed",
+					Message = "Id does not exist"
+				};
+			}
+			return new JsonResponse {
+				Data = db.Users.Find(id)
+			};
 		}
+
+		//POST
 		[HttpPost]
-		public bool Create(User user) {
-			if(user == null) return false;
-			if(!ModelState.IsValid) return false;
+		[ActionName("Create")]
+		public JsonResponse Create(User user) {
+			if(user == null) {
+				return new JsonResponse {
+					Result = "Failed",
+					Message = "Create requires an instance of Major"
+				};
+			}
+			if(!ModelState.IsValid) {
+				return new JsonResponse {
+					Result = "Failed",
+					Message = "Model state is invalid. See data.",
+					Error = ModelState
+				};
+			}
+
 			db.Users.Add(user);
 			db.SaveChanges();
-			return true;
+			return new JsonResponse {
+				Message = "Create successful.",
+				Data = user
+			};
 		}
+
+		//CHANGE
 		[HttpPost]
-		public bool Change(User user) {
-			if(user == null) return false;
-			if(!ModelState.IsValid) return false;
-			db.Users.Attach(user);
+		[ActionName("Change")]
+		public JsonResponse Change(User user) {
+			if(user == null) {
+				return new JsonResponse {
+					Result = "Failed",
+					Message = "Create requires an instance of Major"
+				};
+			}
+			if(!ModelState.IsValid) {
+				return new JsonResponse {
+					Result = "Failed",
+					Message = "Model state is invalid. See data.",
+					Error = ModelState
+				};
+			}
 			db.Entry(user).State = System.Data.Entity.EntityState.Modified;
 			db.SaveChanges();
-			return true;
+			return new JsonResponse {
+				Message = "Change successful.",
+				Data = user
+			};
 		}
+
+		//DELETE
 		[HttpPost]
-		public bool Remove(User user) {
-			if(user == null) return false;
-			if(!ModelState.IsValid) return false;
-			db.Users.Attach(user);
+		[ActionName("Remove")]
+		public JsonResponse Remove(User user) {
+			if(user == null) {
+				return new JsonResponse {
+					Result = "Failed",
+					Message = "Create requires an instance of Major"
+				};
+			}
+			if(!ModelState.IsValid) {
+				return new JsonResponse {
+					Result = "Failed",
+					Message = "Model state is invalid. See data.",
+					Error = ModelState
+				};
+			}
 			db.Entry(user).State = System.Data.Entity.EntityState.Deleted;
 			db.SaveChanges();
-			return true;
+			return new JsonResponse {
+				Message = "Remove successful.",
+				Data = user
+			};
 		}
+
+		//REMOVE/ID
+		[HttpGet]
+		[ActionName("RemoveId")]
+		public JsonResponse Remove(int? id) {
+			if(id == null)
+				return new JsonResponse {
+					Result = "Failed",
+					Message = "RemoveId requires a User.Id"
+				};
+			var user = db.Users.Find(id);
+			if(user == null)
+				return new JsonResponse {
+					Result = "Failed",
+					Message = $"No User has Id of {id}"
+				};
+			db.Users.Remove(user);
+			db.SaveChanges();
+			return new JsonResponse {
+				Message = "Remove successful.",
+				Data = user
+			};
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+		//[HttpGet]
+		//public IEnumerable<User> List() {
+		//	return db.Users.ToList();
+		//}
+		//[HttpGet]
+		//public User Get(int? id) {
+		//	if(id == null) return null;
+		//	return db.Users.Find(id);
+		//}
+		//[HttpPost]
+		//public bool Create(User user) {
+		//	if(user == null) return false;
+		//	if(!ModelState.IsValid) return false;
+		//	db.Users.Add(user);
+		//	db.SaveChanges();
+		//	return true;
+		//}
+		//[HttpPost]
+		//public bool Change(User user) {
+		//	if(user == null) return false;
+		//	if(!ModelState.IsValid) return false;
+		//	db.Users.Attach(user);
+		//	db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+		//	db.SaveChanges();
+		//	return true;
+		//}
+		//[HttpPost]
+		//public bool Remove(User user) {
+		//	if(user == null) return false;
+		//	if(!ModelState.IsValid) return false;
+		//	db.Users.Attach(user);
+		//	db.Entry(user).State = System.Data.Entity.EntityState.Deleted;
+		//	db.SaveChanges();
+		//	return true;
+		//}
 	}
 }
